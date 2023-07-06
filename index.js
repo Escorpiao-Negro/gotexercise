7f2d67c3172d13ca10edad808978f3b6f436f4f4
Ed de Almeida <edvaldoajunior@gmail.com>
2023-07-06 09:04:44 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const APIBASE = 'https://www.anapioficeandfire.com/api'
+
+const endpointsEntry = document.getElementById('endpoints-entrypoint')
+const booksEntrypoint = document.getElementById('books-entrypoint')
+
+async function loadEndpoints () {
+  return await fetch(APIBASE)
+    .then(res => res.json())
+    .then(json => {
+      const lista = Object.keys(json)
+      lista.forEach(l => {
+        let itemName = l.slice(0, 1).toUpperCase() + l.slice(1)
+        const liItem = document.createElement('li')
+        liItem.innerHTML = `<span>${itemName}: <a target="_new" href="${json[l]}">${json[l]}</a></span>`
+        endpointsEntry.appendChild(liItem)
+      })
+    })
+}
+
+async function loadBooks () {
+  return await fetch(APIBASE + '/books')
+    .then(res => res.json())
+    .then(json => {
+      json.forEach(l => {
+        const liItem = document.createElement('li')
+        liItem.innerHTML = `<span>${l.name}</span>`
+        booksEntrypoint.appendChild(liItem)
+      })
+    })
+}
+
+loadEndpoints()
+loadBooks()
